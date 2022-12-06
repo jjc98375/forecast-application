73863be08211f471c254eebeada925b9d9f9145a
Joshua Cho <62311247+jjc98375@users.noreply.github.com>
2022-12-06 09:51:35 -0800

    commit 1

---
+++ react-weather-app/src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'c9c988cc1cmsh99c63bfd5cdd9fcp180c8fjsn0cdf71fc6730',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0ae4f1dbd330cb15a12910300aca783d";
